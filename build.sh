d9e0a03666f8472dd78893b45bada537bcafd181
Ionuț Bizău <i@ibz.me>
2020-08-26 10:42:25 +0800

    Edit content.

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t ibz0/1984.solutions .
--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -e VIRTUAL_HOST=1984.solutions --network=webproxy --name=1984solutions -e LETSENCRYPT_HOST=1984.solutions -e LETSENCRYPT_EMAIL=hi@1984.solutions ibz0/1984.solutions
